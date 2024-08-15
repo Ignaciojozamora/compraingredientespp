611e8b28e5426b9a9c2d531eaac1fb73c861897a
Ignacio Zamora <ignaciojo.zamora@gmail.com>
2024-08-15 10:17:06 -0300

    cart context

--- src/storage/cartContext.jsx
+++ src/storage/cartContext.jsx
@@ -6,14 +6,34 @@ export function CartContextProvider (props) {
     let [cart, setCart] = useState([])
 
     function addItem(receta, count) {
-
-        /* let newCart = [...cart]
-        newCart.push(item)
-        setCart(newCart)  forma alternativa */
+        const isIncart = cart.some(itemInCart => itemInCart.id === receta.id)
+        if(isIncart){
+            let newCart = [...cart];
+            let index = cart.findIndex(itemInCart => itemInCart.id === receta.id)
+            /* newCart[index] */
+        }
+        else {
         setCart([...cart, receta])
+        }    
     }
 
-    const value = {cart, addItem}
+    function removeItem (id) {
+        /* remover item */
+    }
+
+    function clearCart () {
+        /* Borarrar carrito */
+    }
+
+    function getTotalItems () {
+        /* total de recetas en carrito */
+    }
+
+    function getTotalIngredientes () {
+        /* suma de ingredientes */
+    }
+
+    const value = {cart, addItem, removeItem, clearCart, getTotalItems, getTotalIngredientes}
     return <cartContext.Provider value= {value}>
         {props.children}
     </cartContext.Provider>
